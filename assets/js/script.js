ca276fbbc5f3f53d3b998757312c7813ff0f0eb6
totes7 <antonio.manno.7@gmail.com>
2022-11-02 11:15:38 +0000

    Add questions array in script.js, fix display question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
+let questions = [
+    {
+        prompt: 'Which of these teams has won the most World Cups in football history?',
+        answers: ['Brazil', 'Italy', 'France', 'Germany']
+    },
+    {
+        prompt: 'Which of these players has scored more goals in World Cup history?',
+        answers: ['Miroslav Klose', 'Ronaldo', 'Roberto Baggio', 'Lionel Messi']
+    },
+    {
+        prompt: 'Which continent has hosted more World Cups?',
+        answers: ['Europe', 'Asia', 'Africa', 'Americas']
+    },
+    {
+        prompt: 'Who is the only player to win 3 World Cups?',
+        answers: ['Pelé', 'Diego Maradona', 'Francesco Totti', 'Franz Beckenbauer']
+    },
+    {
+        prompt: 'Which of these teams has lost the World Cup final the most times?',
+        answers: ['Germany', 'Argentina', 'England', 'Uruguay']
+    },
+    {
+        prompt: 'Who won the first ever World Cup?',
+        answers: ['Uruguay', 'Italy', 'Brazil', 'Messico']
+    },
+    {
+        prompt: 'Who is the oldest player to play and score in a World Cup at age 42?',
+        answers: ['Roger Milla', 'Miroslav Klose', 'Diego Maradona', 'Michel Platini']
+    },
+    {
+        prompt: 'Who scored the fastest goal in World Cup history, after just 11 seconds?',
+        answers: ['Hakan Sukur', 'Pelé', 'Harry Kane', 'Cristiano Ronaldo']
+    },
+    {
+        prompt: 'Who is the only coach to win 2 World Cups?',
+        answers: ['Vittorio Pozzo', 'Felipe Scolari', 'Fabio Capello', 'Vicente del Bosque']
+    },
+    {
+        prompt: 'Australia won a World Cup qualifying match for 31-0. Who did they win against?',
+        answers: ['American Samoa', 'Fiji', 'Japan', 'New Zeland']
+    },
+]
+
+
+
 // Wait for the DOM to finish loading before starting the quiz
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -25,14 +70,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function displayQuestionOne() {
     let question = document.getElementById('question');
-    question.innerText = 'Which of these teams has won the most World Cups in football history?';
+    question.innerText = questions[0].prompt;
 
     let answerA = document.getElementById('answer-a');
-    answerA.innerText = 'Germany';
+    answerA.innerText = questions[0].answers[0];
     let answerB = document.getElementById('answer-b');
-    answerB.innerText = 'Italy';
+    answerB.innerText = questions[0].answers[1];
     let answerC = document.getElementById('answer-c');
-    answerC.innerText = 'Brazil';
+    answerC.innerText = questions[0].answers[2];
     let answerD = document.getElementById('answer-d');
-    answerD.innerText = 'France';
+    answerD.innerText = questions[0].answers[3];
 }
